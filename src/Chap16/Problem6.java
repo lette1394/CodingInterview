436a5cb08afcfda9980192f9b646b1bcc0fb8792
lette1394 <lette1394+github@gmail.com>
2018-01-06 17:16:27 +0900

    최초 테스트케이스 작성

---
+++ src/Chap16/Problem6.java
@@ -0,0 +1,11 @@
+package Chap16;
+
+public class Problem6 {
+
+    public int getMinAbsValueOfMinusBetween(int[] array1, int[] array2) {
+
+
+
+        return 0;
+    }
+}
---
+++ test/Chap16/Problem6Test.java
@@ -0,0 +1,22 @@
+package Chap16;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Problem6Test {
+    Problem6 p6;
+
+    @Before
+    public void before() {
+        Problem6 p6 = new Problem6();
+    }
+
+    @Test
+    public void finalTest() {
+        assertEquals(3, p6.getMinAbsValueOfMinusBetween(
+                new int[] {1, 3, 15, 11, 2},
+                new int[] {23, 127, 235, 19, 8})
+        );
+    }
+}
