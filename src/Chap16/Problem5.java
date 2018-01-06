2bbe4d7cd0acce976c18ba50a0f667d3b4eb2025
lette1394 <lette1394+github@gmail.com>
2018-01-06 14:38:14 +0900

    팩토리얼 함수

--- src/Chap16/Problem5.java
+++ src/Chap16/Problem5.java
@@ -9,4 +9,17 @@ public class Problem5 {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    public int getFactorial(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        int sum = 1;
+        for (int i = 1; i <= number; i++) {
+            sum *= i;
+        }
+
+        return sum;
+    }
 }
--- test/Chap16/Problem5Test.java
+++ test/Chap16/Problem5Test.java
@@ -1,16 +1,27 @@
 package Chap16;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Problem5Test {
 
-    @Test(timeout = 20)
-    public void helloWorld() {
-        assertEquals("hello world!", Problem5.ex1());
+    Problem5 problem5;
+
+    @Before
+    public void before() {
+        this.problem5 = new Problem5();
     }
 
-
+    @Test
+    public void 계승_계산() {
+        assertEquals(1, problem5.getFactorial(0));
+        assertEquals(1, problem5.getFactorial(1));
+        assertEquals(2, problem5.getFactorial(2));
+        assertEquals(6, problem5.getFactorial(3));
+        assertEquals(24, problem5.getFactorial(4));
+        assertEquals(120, problem5.getFactorial(5));
+    }
 }
 
 
