b3b708082b1bc7c55da83d4ef1173282e7fbc96f
lette1394 <lette1394+github@gmail.com>
2018-01-10 14:49:23 +0900

    Problem8.1 - cache

--- src/Chap8/Problem1.java
+++ src/Chap8/Problem1.java
@@ -2,11 +2,12 @@ package Chap8;
 
 public class Problem1 {
 
+    int[] cache = new int[1000];
+
     int climbStairs(int stairs) {
         if (stairs == 0) {
             return 0;
         }
-
         return goUp(stairs);
     }
 
@@ -18,7 +19,10 @@ public class Problem1 {
             return 1;
         }
         else {
-            return goUp(remain-1) + goUp(remain-2) + goUp(remain-3);
+            if (cache[remain] != 0) {
+                return cache[remain];
+            }
+            return cache[remain] = goUp(remain-1) + goUp(remain-2) + goUp(remain-3);
         }
     }
 }
