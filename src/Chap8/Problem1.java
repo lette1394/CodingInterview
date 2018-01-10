4c450da7d41efe2deaec974198b78927c30996c3
lette1394 <lette1394+github@gmail.com>
2018-01-10 14:45:14 +0900

    Problem8.1 - recursion

--- src/Chap8/Problem1.java
+++ src/Chap8/Problem1.java
@@ -1,4 +1,25 @@
 package Chap8;
 
 public class Problem1 {
+
+    int climbStairs(int stairs) {
+        if (stairs == 0) {
+            return 0;
+        }
+
+        return goUp(stairs);
+    }
+
+    int goUp(int remain) {
+        if (remain < 0){
+            return 0;
+        }
+        if (remain == 0) {
+            return 1;
+        }
+        else {
+            return goUp(remain-1) + goUp(remain-2) + goUp(remain-3);
+        }
+    }
 }
+
--- test/Chap8/Problem1Test.java
+++ test/Chap8/Problem1Test.java
@@ -1,4 +1,35 @@
 package Chap8;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class Problem1Test {
+    Problem1 p1;
+
+    @Before
+    public void before() {
+        p1 = new Problem1();
+    }
+
+    @Test
+    public void zero() {
+        assertEquals(0, p1.climbStairs(0));
+    }
+
+    @Test
+    public void moreThanOne() {
+        assertEquals(1, p1.climbStairs(1));
+        assertEquals(2, p1.climbStairs(2));
+        assertEquals(4, p1.climbStairs(3));
+        assertEquals(7, p1.climbStairs(4));
+        assertEquals(13, p1.climbStairs(5));
+        assertEquals(24, p1.climbStairs(6));
+        assertEquals(44, p1.climbStairs(7));
+        assertEquals(81, p1.climbStairs(8));
+        assertEquals(149, p1.climbStairs(9));
+        assertEquals(274, p1.climbStairs(10));
+    }
 }
