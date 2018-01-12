26f36d264d467339a77861ce8fb27f4e6f97464a
lette1394 <lette1394+github@gmail.com>
2018-01-12 14:20:44 +0900

    [problem10.5] sparseSearch 2

--- src/Chap10/Problem5.java
+++ src/Chap10/Problem5.java
@@ -3,21 +3,43 @@ package Chap10;
 public class Problem5 {
 
 
-    int sparseSearch(String target, String[] strings, int left, int right) {
-        int index = partition(target, strings, left, right);
-
-
-
-        return index;
+    int sparseSearch(String target, String[] strings) {
+        return sparseSearch(target, strings, 0, strings.length-1);
     }
 
-    int partition(String target, String[] strings, int left, int right) {
+    int sparseSearch(String target, String[] strings, int left, int right) {
+        int rightMid = (left + right) / 2 ;
+        int leftMid = (left + right) / 2 ;
         int mid = (left + right) / 2 ;
 
-        while(strings[mid].equals("")) {
-            mid++;
+        if (target.equals(strings[mid])) {
+            return mid;
         }
 
-        return target.compareTo(strings[mid]);
+
+        while(strings[rightMid].equals("") && rightMid <= right) {
+            rightMid++;
+        }
+        if (strings[rightMid].equals(target)) {
+            return rightMid;
+        }
+
+        while(strings[leftMid].equals("") && left <= leftMid) {
+            leftMid--;
+        }
+        if (strings[leftMid].equals(target)) {
+            return leftMid;
+        }
+
+
+        if (target.compareTo(strings[leftMid]) < 0) {
+            return sparseSearch(target, strings, left, leftMid);
+        }
+        else if (target.compareTo(strings[rightMid]) > 0) {
+            return sparseSearch(target, strings, rightMid + 1, right);
+        }
+        else {
+            return sparseSearch(target, strings, leftMid + 1, rightMid);
+        }
     }
 }
--- test/Chap10/Problem5Test.java
+++ test/Chap10/Problem5Test.java
@@ -14,28 +14,14 @@ public class Problem5Test {
         this.p5 = new Problem5();
     }
 
-    @Ignore
     @Test
     public void main() {
         String[] str = new String[] {
                 "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""
         };
-        assertEquals(4, p5.sparseSearch("ball", str, 0, str.length-1));
+        assertEquals(4, p5.sparseSearch("ball", str));
     }
 
     @Test
-    public void LeftPartition() {
-        String[] str = new String[] {
-                "abc", "", "cdd", "", ""
-        };
-        assertEquals(-1, p5.partition("ccc", str, 0, str.length-1));
-    }
 
-    @Test
-    public void RightPartition() {
-        String[] str = new String[] {
-                "abc", "", "cdd", "", ""
-        };
-        assertEquals(1, p5.partition("ddd", str, 0, str.length-1));
-    }
 }
