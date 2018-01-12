62842cb8e9508ba6fa3b0c604088166741ef54aa
lette1394 <lette1394+github@gmail.com>
2018-01-12 14:26:13 +0900

    [problem10.5] add Tests

--- test/Chap10/Problem5Test.java
+++ test/Chap10/Problem5Test.java
@@ -23,5 +23,56 @@ public class Problem5Test {
     }
 
     @Test
+    public void boundary() {
+        String[] str1 = new String[] {
+                "at"
+        };
+        assertEquals(0, p5.sparseSearch("at", str1));
+
+        String[] str2 = new String[] {
+                "two"
+        };
+        assertEquals(0, p5.sparseSearch("two", str2));
+
+        String[] blank = new String[] {
+                ""
+        };
+        assertEquals(0, p5.sparseSearch("", blank));
+    }
+
+    @Test
+    public void two() {
+        String[] str1 = new String[] {
+                "at", "", "kk"
+        };
+        assertEquals(2, p5.sparseSearch("kk", str1));
+
+        String[] str2 = new String[] {
+                "", "", "", "zero"
+        };
+        assertEquals(3, p5.sparseSearch("zero", str2));
+    }
+
+    @Test
+    public void length() {
+        String[] str1 = new String[] {
+                "at", "", "kk", "", "kkk", "", "", "", "kkkk", "", "kkkkk", "", "", "kkkkkkk", "", "kkkkkkkkk", ""
+        };
+        assertEquals(8, p5.sparseSearch("kkkk", str1));
+        assertEquals(10, p5.sparseSearch("kkkkk", str1));
+        assertEquals(13, p5.sparseSearch("kkkkkkk", str1));
+    }
+
+    @Test
+    public void order() {
+        String[] str1 = new String[] {
+                "", "", "aaabb", "", "aaabc", "", "", "", "aabbc", "", "aabce", "", "", "aaccc", "", "abcd", "", "", "", ""
+        };
+        assertEquals(4, p5.sparseSearch("aaabc", str1));
+        assertEquals(8, p5.sparseSearch("aabbc", str1));
+        assertEquals(15, p5.sparseSearch("abcd", str1));
+    }
+
+
 
 }
