d5a072cba37d5327438f1f57222a54cd49ddc60e
lette1394 <lette1394+github@gmail.com>
2018-01-13 21:07:35 +0900

    [UnderStairs] recursion ADD main

--- src/Programmers/UnderStairs.java
+++ src/Programmers/UnderStairs.java
@@ -1,79 +1,44 @@
 package Programmers;
 
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class UnderStairs {
-    class Node {
-        int x, y, tilValue;
-        Node(int x, int y, int tilValue) {
-            this.x = x;
-            this.y = y;
-            this.tilValue = tilValue;
-        }
-    }
-
     int[][] grid;
-    int[][] cache;
-
-    Stack<Node> stack = new Stack<>();
 
     int count(int[][] grid) {
         this.grid = grid;
-        this.cache = new int[grid.length][grid[0].length];
 
-        for (int i = 0; i < cache.length; i++) {
-            Arrays.fill(cache[i], -1);
-        }
         return count(0, 0, 999999);
     }
 
-    int count(int X, int Y, int PRE) {
-        int result = 1;
-        Node current;
-        stack.push(new Node(X, Y, PRE));
+    int count(int x, int y, int pre) {
+        int result = 0;
 
-        while (!stack.empty()) {
-            current = stack.pop();
-            int x = current.x;
-            int y = current.y;
-            int beforeValue = current.tilValue;
-            int currentValue = grid[y][x];
+        if (!isDownSide(x, y, pre)) {
+            return 0;
+        }
 
-            if (cache[y][x] == 0) {
-                continue;
-            }
-            else if (cache[y][x] > 0) {
-                result *= cache[y][x];
-            }
+        if (isArrived(x, y)) {
+            return 1;
+        }
 
-            if (isArrived(x, y)) {
-                cache[y][x] = 1;
-                continue;
-            }
-
-            if (isValid(x+1, y, beforeValue)) {
-                stack.push(new Node(x+1, y, currentValue));
-            }
-            if (isValid(x-1, y, beforeValue)) {
-                stack.push(new Node(x-1, y, currentValue));
-            }
-            if (isValid(x, y+1, beforeValue)) {
-                stack.push(new Node(x, y+1, currentValue));
-            }
-            if (isValid(x, y-1, beforeValue)) {
-                stack.push(new Node(x, y-1, currentValue));
-            }
+        if (isValid(x+1, y)) {
+            result += count(x+1, y, grid[y][x]);
+        }
+        if (isValid(x-1, y)) {
+            result += count(x-1, y, grid[y][x]);
+        }
+        if (isValid(x, y+1)) {
+            result += count(x, y+1, grid[y][x]);
+        }
+        if (isValid(x, y-1)) {
+            result += count(x, y-1, grid[y][x]);
         }
         return result;
     }
 
-    private boolean isValid(int x, int y, int beforeValue) {
-        return isUnderBoundary(x, y) && isToDownSide(x, y, beforeValue);
-    }
-
-    private boolean isUnderBoundary(int x, int y) {
+    private boolean isValid(int x, int y) {
         return 0 <= x && x < grid[0].length && 0 <= y && y < grid.length;
     }
 
@@ -81,7 +46,7 @@ public class UnderStairs {
         return x == grid[0].length-1 && y == grid.length-1;
     }
 
-    private boolean isToDownSide(int x, int y, int pre) {
+    private boolean isDownSide(int x, int y, int pre) {
         return grid[y][x] < pre;
     }
 
