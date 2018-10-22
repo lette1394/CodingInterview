20d8e9796f4f17868bd800bbdd5da9fde20986fd
Na <lette1394+github@gmail.com>
2018-10-22 19:29:12 +0900

    ADD iterate test for billion

--- src/Practice/MySort.java
+++ src/Practice/MySort.java
@@ -28,4 +28,36 @@ public class MySort {
             }
         }
     }
+
+    static void quickSort(int[] arr) {
+        _quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void _quickSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+
+        int lo = l;
+        int hi = r;
+        int mid = (lo + hi) / 2;
+
+        while (lo < hi) {
+            while (arr[lo] < arr[mid]) lo++;
+            while (arr[hi] > arr[mid]) hi--;
+
+            if (lo < hi) {
+                swap(arr, lo++, hi--);
+            }
+        }
+        _quickSort(arr, l, mid - 1);
+        _quickSort(arr, mid + 1, r);
+    }
+
+    static int iterate(long times) {
+        int sum = 0;
+        for (int i = 0; i < times; i++) {
+            sum++;
+        }
+
+        return sum;
+    }
 }
--- test/Practice/MySortTest.java
+++ test/Practice/MySortTest.java
@@ -2,9 +2,12 @@ package Practice;
 
 import org.junit.jupiter.api.Test;
 
-import static Practice.MySort.insertSort;
-import static Practice.MySort.selectionSort;
+import java.time.Duration;
+
+import static Practice.MySort.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 class MySortTest {
 
@@ -70,15 +73,48 @@ class MySortTest {
     }
 
     @Test
+    void quick_sort_1() throws Exception {
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+
+        quickSort(arr);
+
+        assertThat(arr).isEqualTo(expected);
+    }
+
+    @Test
+    void quick_sort_2() throws Exception {
+        int[] arr = {5, 4, 3, 2, 1, 1, 24, 5, 6, 789, 44};
+        int[] expected = {1, 1, 2, 3, 4, 5, 5, 6, 24, 44, 789};
+
+        quickSort(arr);
+        assertThat(arr).isEqualTo(expected);
+    }
+
+    @Test
     void handles_empty_arr() {
         int[] arr1 = {};
         int[] arr2 = {};
+        int[] arr3 = {};
         int[] expected = {};
 
         selectionSort(arr1);
         insertSort(arr2);
-        
+        quickSort(arr3);
+
         assertThat(arr1).isEqualTo(expected);
         assertThat(arr2).isEqualTo(expected);
+        assertThat(arr3).isEqualTo(expected);
+    }
+
+    @Test
+    void should_run_in_1_second() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            System.out.println(iterate(100000000));
+        });
+
+        assertTimeoutPreemptively(Duration.ofMillis(1000), () -> {
+            System.out.println(iterate(1000000000L));
+        });
     }
 }
