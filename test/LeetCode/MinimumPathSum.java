737bb500933af88b4c766f0a8272a703bcafd0ea
Na <lette1394+github@gmail.com>
2018-10-26 20:04:42 +0900

    ADD Solution to MinimumPathSum - iteration

--- test/LeetCode/MinimumPathSum.java
+++ test/LeetCode/MinimumPathSum.java
@@ -49,6 +49,29 @@ public class MinimumPathSum {
 
 
     // should rewrite above code to iteration code. (typically called DP solution)
+    class Solution {
+        public int minPathSum(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
 
+            int[][] dp = new int[m+2][n+2];
+
+            dp[0][0] = grid[0][0];
+            for (int i = 1; i < grid.length; i++) {
+                dp[i][0] = dp[i-1][0] + grid[i][0];
+            }
+            for (int i = 1; i< grid[0].length; i++) {
+                dp[0][i] = dp[0][i-1] + grid[0][i];
+            }
+
+            for (int y = 1; y < m; y++) {
+                for (int x = 1; x < n; x++) {
+                    dp[y][x] = Math.min(dp[y-1][x], dp[y][x-1]) + grid[y][x];
+                }
+            }
+
+            return dp[m-1][n-1];
+        }
+    }
 
 }
